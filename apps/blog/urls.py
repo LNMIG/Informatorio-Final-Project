8097f4c41123826a3e6b0303bd25ab0fc4c36850
LNMIG <migliorenicolas@yahoo.com.ar>
2023-07-15 15:01:22 -0300

    Fix: carpeta del proyecto 'core' modificada para eficiencia y archivos de la app 'blog' modificados en su url'

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('autor/', views.autor, name='autor'),
     path('archivo/', views.archivo, name='archivo'),
-    path('articulo/', views.categoria, name='articulo'),
+    path('articulo/', views.articulo, name='articulo'),
     path('categoria/', views.categoria, name='categoria'),
 ]
