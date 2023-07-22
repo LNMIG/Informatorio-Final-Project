ce0b7417363ca7016d8e321f1e3448999f7ac2cb
priscibercheni <93732671+priscibercheni@users.noreply.github.com>
2023-07-21 21:07:11 -0300

    Se agregan modelos

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+admin.site.site_header = 'Administración del Blog'
+admin.site.index_title = 'Panel de Control'
+admin.site.site_title = 'Blog'
 
 # Register your models here.
