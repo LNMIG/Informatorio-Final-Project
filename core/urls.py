fe54cbf2a75c89754bf3145bed576e0c97546e3e
priscibercheni <93732671+priscibercheni@users.noreply.github.com>
2023-07-22 11:21:21 -0300

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+from apps.blog.views import NotFoundView
+
+
+handler404 = NotFoundView.as_view()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
