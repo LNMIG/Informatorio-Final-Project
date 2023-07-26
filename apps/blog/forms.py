d13b5589098aa0b08010709970b0bab68d20f161
LangBraian <braianelang@gmail.com>
2023-07-26 20:22:58 -0300

    Se agrego la verificacion de correo electronico

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 from . import models
 
@@ -17,3 +19,36 @@ class ArticuloForm(forms.ModelForm):
             'categoria': forms.Select(attrs={'class': 'form-control'}),
             'etiquetas': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
+
+class RegisterUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =  ['username', 'email', 'password1', 'password2']
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Usuario',
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email',
+    }))
+    password1= forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Contraseña',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Repetir Contraseña',
+    }))
+
+    def clean_email(self):
+        email_recibido = self.cleaned_data.get("email")
+        correo_ya_registrado = User.objects.filter(email = email_recibido).exists()
+        user_es_activo = User.objects.filter(email = email_recibido, is_active = 1)
+        if correo_ya_registrado and user_es_activo:
+            raise forms.ValidationError("Correo electrónico ya registrado.")
+        elif correo_ya_registrado:
+            User.objects.filter(email = email_recibido).delete()
+
+        return email_recibido
